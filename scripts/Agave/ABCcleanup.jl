81c6a1b2dc10283269e316bf7c982803e558f87a
Manuela Vanegas Ferro <mvanega1@asu.edu>
2021-06-17 20:19:47 -0700

    Test raw file integrity

--- scripts/Agave/ABCcleanup.jl
+++ scripts/Agave/ABCcleanup.jl
@@ -1,11 +1,30 @@
-@everywhere using DrWatson
-@everywhere @quickactivate "SpatialRust"
+# @everywhere using DrWatson
+# @everywhere @quickactivate "SpatialRust"
+#
+# @everywhere using CSV, DataFrames, Distributed, StatsBase
+# @everywhere include(srcdir("ABCproc.jl"))
+#     #include(srcdir("ABCrun.jl"))
+#
+#
+# load_to_select("/scratch/mvanega1/ABCveryraw/", ARGS[1], parse(Int, ARGS[2]), parse(Int, ARGS[3]))
+#
+# #filter_params("/scratch/mvanega1/ABCveryraw/")
 
-@everywhere using CSV, DataFrames, Distributed, StatsBase
-@everywhere include(srcdir("ABCproc.jl"))
-    #include(srcdir("ABCrun.jl"))
+using DrWatson
+@quickactivate "SpatialRust"
 
+using CSV, DataFrames
 
-load_to_select("/scratch/mvanega1/ABCveryraw/", ARGS[1], parse(Int, ARGS[2]), parse(Int, ARGS[3]))
-
-#filter_params("/scratch/mvanega1/ABCveryraw/")
+startedat = time()
+outfolder = mkpath(projectdir("results","faulty"))
+files = readdir("/scratch/mvanega1/ABCveryraw/", join = true, sort = false)
+for f in files
+    missing_vals = ismissing.(CSV.read(f, DataFrame))
+    println(basename(f))
+    now = time() - startedat
+    println(now)
+    sums = sum(eachcol(missing_vals))
+    if any(sums .> 0)
+        CSV.write(string(outfolder, basename(f)), DataFrame(v = sums))
+    end
+end
