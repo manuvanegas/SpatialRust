c1c1b170792440c55a0bf0dd1dff7a9f1f7e1927
Manuela Vanegas Ferro <mvanega1@asu.edu>
2022-04-25 23:43:26 -0700

    Update LocalTestRuns.jl

--- scripts/ABC/LocalTestRuns.jl
+++ scripts/ABC/LocalTestRuns.jl
@@ -8,31 +8,15 @@ using CSV: read as crd
 when_rust = Vector(Arrow.Table("data/exp_pro/inputs/sun_whentocollect_rust.arrow")[1])
 const when_plant = Vector(Arrow.Table("data/exp_pro/inputs/sun_whentocollect_plant.arrow")[1])
 
-# const when_rust = sort!(union(when_plant, when_rust))
+# const when_rust = sort!(union(when_plant, when_rust)) # needed because the first 5 dates of when_prod are not in when_rust
 
 # read climate data
 const rain_data = Vector(Arrow.Table("data/exp_pro/inputs/sun_weather.arrow")[1])
 const temp_data = Vector(Arrow.Table("data/exp_pro/inputs/sun_weather.arrow")[2])
 const paramss = crd(datadir("ABC", "parameters_10.csv"), DataFrame)
-# for i in when_plant; println(i in when_rust);end
-# false
-# false
-# false
-# false
-# false
-# true
-# true
-# true
-# true
-# true
-
 
 include(projectdir("SpatialRust.jl"))
 using .SpatialRust
-
-pars = Parameters(steps = 231, map_side = 100, switch_cycles = copy(when_plant))
-ttmodel = init_spatialrust(pars, Main.SpatialRust.create_fullsun_farm_map(), Main.SpatialRust.create_weather(pars.rain_prob, pars.wind_prob, pars.mean_temp, pars.steps))
-Main.SpatialRust.custom_sampling_second!(ttmodel, 0.05)
 
 # Random.seed!(1234)
 # for rr in eachrow(paramss)[1:2]
@@ -47,6 +31,11 @@ Random.seed!(1234)
     $(eachrow(paramss)[1]), $rain_data, $temp_data,
     $when_rust, $when_plant, 0.5, 231)
 
+for i in 1:10
+    println(i)
+    sim_abc(eachrow(paramss)[1], rain_data, temp_data, when_rust, when_plant, 0.5, 231)
+end
+
 @btime sim_abc($(eachrow(paramss)[4]), $rain_data, $temp_data, $when_rust, $when_plant, 0.5, 231)
 # 55 s!
 
@@ -60,6 +49,21 @@ SpatialRust.abc_run!(tmodel,
     obtainer = identity)
 
 ##
+pars = Parameters(steps = 231, map_side = 100, switch_cycles = copy(when_plant))
+ttmodel = init_spatialrust(pars, Main.SpatialRust.create_fullsun_farm_map(), Main.SpatialRust.create_weather(pars.rain_prob, pars.wind_prob, pars.mean_temp, pars.steps))
+Main.SpatialRust.custom_sampling_second!(ttmodel, 0.05)
+
+Random.seed!(1234)
+pars = Parameters(steps = 231, map_side = 100, switch_cycles = copy(when_plant))
+for i in 1:500
+    ttmodel = init_spatialrust(pars, Main.SpatialRust.create_fullsun_farm_map(), Main.SpatialRust.create_weather(pars.rain_prob, pars.wind_prob, pars.mean_temp, pars.steps))
+    Main.SpatialRust.custom_sampling_second!(ttmodel, 0.05)
+    if isempty(Iterators.filter(c -> c isa Coffee && 8 in c.sample_cycle, allagents(ttmodel)))
+        println(i)
+        break
+    end
+end
+
 using DataFrames
 tmodel = justtwosteps()
 SpatialRust.custom_sampling_first!(tmodel, 0.05)
