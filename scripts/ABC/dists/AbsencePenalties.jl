261d82f786130f98a12a205cf36df75f0fd992ed
Manuela Vanegas Ferro <mvanega1@asu.edu>
2022-04-26 23:37:22 -0700

    Update AbsencePenalties.jl

--- scripts/ABC/dists/AbsencePenalties.jl
+++ scripts/ABC/dists/AbsencePenalties.jl
@@ -7,15 +7,17 @@ cyclesfiles = readdir("/scratch/mvanega1/ABC/sims/cycles/", join = true, sort = 
 
 allfiles = vcat(agesfiles, cyclesfiles)
 
-@everywhere function penalties!(file::String)
+@everywhere function penalties!(file::String, num::Int)
     df = DataFrame(Arrow.Table(file))
     replace!(df[!, :area_m], NaN => -1.0)
     replace!(df[!, :spores_m], NaN => -1.0)
     Arrow.write(file, df)
     df = nothing
+    println(num)
+    flush(stdout)
     GC.gc()
     return nothing
 end
 
 # penalties!.(agesfiles)
-pmap(penalties!, allfiles; retry_delays = fill(0.1, 3))
+v = pmap((i,f) -> penalties!(f,i), enumerate(allfiles); retry_delays = fill(0.1, 3))
