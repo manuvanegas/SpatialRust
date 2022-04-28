782b415d56b478e4d52f6bb2d7dc5c164ad61c43
Manuela Vanegas Ferro <mvanega1@asu.edu>
2022-04-28 00:29:48 -0700

    Plot ABC distributions

--- src/ABC/PlotDistributions.jl
+++ src/ABC/PlotDistributions.jl
@@ -1,24 +1,37 @@
-using Arrow, CSV, DataFrames, StatsPlots
+Pkg.activate(".")
+using Arrow, CairoMakie, CSV, DataFrames
 
-include("Ranks.jl")
+violin(repeat([1,2], 10), 1:20)
+boxplot(repeat([1,2], 10), 1:20)
 
-dists = CSV.read("results/ABC/dists.csv", DataFrame)
+include("RanksPlots.jl")
 
 parameters = DataFrame(Arrow.Table(string("data/ABC/", "parameters_", 10^6, ".arrow")))
+dists = CSV.read("results/ABC/dists/hand_dists.csv", DataFrame)
+selected = subset(parameters, :RowN => x -> x .∈ Ref(best_n(dists, metrics(1), 100)))
+# selected2 = subset(parameters, :RowN => x -> x .∈ Ref(best_n(dists, metrics(2), 100)))
+# selected3 = subset(parameters, :RowN => x -> x .∈ Ref(best_n(dists, metrics(3), 100)))
+# selected4 = subset(parameters, :RowN => x -> x .∈ Ref(best_n(dists, metrics(4), 100)))
 
-thenames = names(parameters)[2:10]
-@df parameters[1:100,:] violin(cols(2:10), ticks= thenames)
+longpars = long_and_separate(parameters)
+longsel = long_and_separate(selected)
+# longsel2 = long_and_separate(selected2)
+# longsel3 = long_and_separate(selected3)
+# longsel4 = long_and_separate(selected4)
 
-@df parameters[1:100,:] boxplot(y = cols(2:10))
 
-@df parameters[1:100, :] violin(xlabel = "opt", :opt_g_temp)
+fig1 = three_boxplots(longpars, longsel)
+Label(fig1[0,:], "With areas per age", textsize = 18)
+fig1
+fig2 = three_boxplots(longpars, longsel2)
+Label(fig2[0,:], "With areas per age", textsize = 18)
+fig2
+fig3 = three_boxplots(longpars, longsel3)
+Label(fig3[0,:], "With sum of areas", textsize = 18)
+fig3
+fig4 = three_boxplots(longpars, longsel4)
+Label(fig4[0,:], "With sum of areas", textsize = 18)
+fig4
 
-
-@df parameters[1:100, :] violin(:opt_g_temp)
-@df parameters[1:100, :] violin!(:max_cof_gr)
-
-using CairoMakie
-
-sparam = stack(parameters)
-
-@df sparam violin(:variable, :value)
+save("plots/ABC/areas_age.png", fig2)
+save("plots/ABC/sum_areas.png", fig4)
---
+++ src/ABC/RanksPlots.jl
@@ -0,0 +1,142 @@
+function best_100(dists::DataFrame, metrics::Vector{Symbol})::Vector{Int}
+    d1 = select(dists, :p_row, AsTable(metrics) => ByRow(sum) => :total_dist)
+    return sort!(d1, :total_dist)[1:100, :p_row]
+end
+
+function best_n(dists::DataFrame, metrics::Vector{Symbol}, n::Int)::Vector{Int}
+    d1 = select(dists, :p_row, AsTable(metrics) => ByRow(sum) => :total_dist)
+    return sort!(d1, :total_dist)[1:n, :p_row]
+end
+
+function metrics(h::Int)
+    if h == 1
+        return [:area_age, :spore_age, :fallen, :prod_fruit]
+    elseif h == 2
+        return [:area_age, :spore_age, :fallen, :prod_node]
+    elseif h == 3
+        return [:area_cycle, :spore_cycle, :fallen, :prod_fruit]
+    else
+        return [:area_cycle, :spore_cycle, :fallen, :prod_node]
+    end
+end
+
+## Plots
+
+function three_violins(pars::NamedTuple, sel::NamedTuple, height = 800, width = 400)
+    par1, par2, par3 = pars
+    sel1, sel2, sel3 = sel
+
+    fviolins = Figure(resolution = (height, width));
+    ax1 = Axis(fviolins[1,1];
+        xticks = (1:7, names(parameters)[2:8]),
+        xticklabelrotation = π/4
+    )
+    ax2 = Axis(fviolins[1,2];
+        xticks = ([1], [names(parameters)[9]]),
+        xticklabelrotation = π/4
+    )
+    ax3 = Axis(fviolins[1,3];
+        xticks = ([1], [names(parameters)[10]]),
+        xticklabelrotation = π/4
+    )
+
+    violin!(ax1, sel1.group, sel1.value, side = :right, color = :teal)
+    violin!(ax1, par1.group, par1.value, side = :left, color = :orange)
+
+    violin!(ax2, sel2.group, sel2.value, side = :right, color = :teal)
+    violin!(ax2, par2.group, par2.value, side = :left, color = :orange)
+
+    violin!(ax3, sel3.group, sel3.value, side = :right, color = :teal)
+    violin!(ax3, par3.group, par3.value, side = :left, color = :orange)
+
+    colsize!(fviolins.layout, 1, Relative(15/18))
+
+    return fviolins
+end
+
+function three_boxplots(pars::NamedTuple, sel::NamedTuple, height = 800, width = 400)
+    par1, par2, par3 = pars
+    sel1, sel2, sel3 = sel
+
+    fbox = Figure(resolution = (height, width));
+    ax1 = Axis(fbox[1,1];
+        xticks = (2:2:14, names(parameters)[2:8]),
+        xticklabelrotation = π/4,
+        ylabel = "Value"
+    )
+    ax2 = Axis(fbox[1,2];
+        xticks = ([1], [names(parameters)[9]]),
+        xticklabelrotation = π/4
+    )
+    xlims! = (ax2,-2, 2)
+    ax3 = Axis(fbox[1,3];
+        xticks = ([1], [names(parameters)[10]]),
+        yticks = (0:5:25),
+        xticklabelrotation = π/4
+    )
+    # Label(fbox[2,:], "Parameter name")
+
+    boxplot!(ax1, sel1.group, sel1.value, dodge = 2, dodge_gap = 2.4, color = (:teal, 0.8))
+    boxplot!(ax1, par1.group, par1.value, dodge = 1, dodge_gap = 2.4, color = (:orange, 0.8))
+
+    boxplot!(ax2, sel2.group, sel2.value, dodge = 2, dodge_gap = 2.4, color = (:teal, 0.8))
+    boxplot!(ax2, par2.group, par2.value, dodge = 1, dodge_gap = 2.4, color = (:orange, 0.8))
+
+    boxplot!(ax3, sel3.group, sel3.value, dodge = 2, dodge_gap = 2.4, color = (:teal, 0.8))
+    boxplot!(ax3, par3.group, par3.value, dodge = 1, dodge_gap = 2.4, color = (:orange, 0.8))
+
+    colsize!(fbox.layout, 1, Relative(15/18))
+    colgap!(fig1.layout, 5)
+    fbox
+end
+
+## Arranging data
+
+function long_and_separate(df::DataFrame)::NamedTuple
+    df1 = select(df, 1:8)
+    df2 = select(df, [1,9])
+    df3 = select(df, [1,10])
+
+    ldf1 = stack(df1)
+    transform!(ldf1, :variable => ByRow(whichgroup) => :group)
+    ldf1[:,:group] .= ldf1[!, :group] .* 2
+    ldf2 = stack(df2)
+    ldf2[:, :group] .= 1
+    ldf3 = stack(df3)
+    ldf3[:, :group] .= 1
+
+    return (g1 = ldf1, g2 = ldf2, g3 = ldf3)
+end
+
+function whichgroup(var)
+    return findfirst(x -> x == var, ("rust_gr", "cof_gr",
+    "spore_pct", "fruit_load", "uv_inact", "rain_washoff",
+    "rain_distance", "wind_distance", "exhaustion"))
+end
+
+######################_____________________________
+# testing stuff
+# using CSV
+# dists = CSV.read("results/ABC/dists.csv", DataFrame)
+# describe(dists)
+#
+# ["p_row", "area_age", "spore_age", "area_cycle", "spore_cycle", "fallen", "prod_fruit", "prod_node"]
+#
+# ttrows = best_100(dists, age_fallen_fruits())
+# ttrows2 = best_100(dists, age_fallen_nodes())
+#
+# setdiff(ttrows, ttrows2)
+# all(ttrows .== ttrows2)
+#
+# ttcrows = best_100(dists, age_fallen_fruits())
+# ttcrows2 = best_100(dists, age_fallen_nodes())
+#
+# setdiff(ttcrows, ttcrows2)
+# all(ttcrows .== ttcrows2)
+#
+#
+# setdiff(ttrows, ttcrows)
+#
+# ttage = best_100(dists, [:area_age, :spore_age])
+# ttcycle = best_100(dists, [:area_cycle, :spore_cycle])
+# setdiff(ttage, ttcycle)
