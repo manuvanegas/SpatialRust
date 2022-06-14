47e7898d7515c7e6137bc7df45c44976f8ddd531
manuvanegas <mvanega1@asu.edu>
2022-06-13 21:52:28 -0700

    Change temp and rain conditions of shading exp

--- scripts/ShadingExperiments/arrayjob-exp.sh
+++ scripts/ShadingExperiments/arrayjob-exp.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# #SBATCH --array=1-3
+#SBATCH --array=1-3
 #SBATCH --ntasks-per-core=1
 
-#SBATCH --ntasks=10
-#SBATCH -p debug
-#SBATCH -q wildfire
-#SBATCH -J debug-shadeexp
-#SBATCH -t 0-00:15:00
-##SBATCH --ntasks=35
-##SBATCH -J shadeexp
-##SBATCH -t 0-01:45:00
+##SBATCH --ntasks=10
+##SBATCH -p debug
+##SBATCH -q wildfire
+##SBATCH -J debug-shadeexp
+##SBATCH -t 0-00:15:00
+#SBATCH --ntasks=35
+#SBATCH -J shadeexp
+#SBATCH -t 0-00:55:00
 
-#SBATCH -o logs/shading/o-%A-%a.o
-#SBATCH -e logs/shading/o-%A.e
+#SBATCH -o logs/shading/o-%x-%A-%a.o
+#SBATCH -e logs/shading/o-%x-%A.e
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mvanega1@asu.edu
 
@@ -23,6 +23,6 @@ module load julia/1.7.2
 export SLURM_NODEFILE=`generate_pbs_nodefile`
 echo `date +%F-%T`
 julia --machine-file $SLURM_NODEFILE \
-~/SpatialRust/scripts/ShadingExperiments/RunExperiment.jl 200 20.0 0.55 $SLURM_ARRAY_TASK_ID
+~/SpatialRust/scripts/ShadingExperiments/RunExperiment.jl 200 22.5 0.55 $SLURM_ARRAY_TASK_ID
 #ARGs: repetitions, mean temp, rain prob, array ID -> fragments
 # 1 -> 29, 2 or 3 -> 42
