056541f58df8b156b4fc9b9c80fd994f75ccc5a4
Manuela Vanegas Ferro <mvanega1@asu.edu>
2023-02-24 22:39:12 -0700

    Use PackageCompiler in batch mode

--- scripts/ABC/sims/precompile-job.sh
+++ scripts/ABC/sims/precompile-job.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-core=1
+#SBATCH --mem=25G
 #SBATCH -p htc
-#SBATCH -q debug
-#SBATCH -J debug-ABC
-#SBATCH -o logs/ABC/prec-%A.o
-#SBATCH -e logs/ABC/prec-%A.e
-#SBATCH -t 0-00:15:00
+#SBATCH -q public
+#SBATCH -J PkgCompiler
+#SBATCH -o logs/ABC/pkgcomp/o.%x-%A.o
+#SBATCH -e logs/ABC/pkgcomp/o.%x-%A.e
+#SBATCH -t 0-00:35:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mvanega1@asu.edu
 
 module purge
 module load julia/1.8.2
 
+ulimit -s 262144
+
 julia << EOF
 using Pkg
 Pkg.activate(".")
 Pkg.instantiate()
 Pkg.precompile()
-using SpatialRust
-dummyrun_spatialrust()
+using PackageCompiler
+PackageCompiler.create_sysimage(["DataFrames", "Arrow", "SpatialRust"];
+	sysimage_path="src/PkgCompile/ABCSysimage.so")
+EOF
+
+julia << EOF
+using Pkg
+Pkg.activate(".")
+using PackageCompiler
+PackageCompiler.create_sysimage(["DataFrames", "Arrow", "SpatialRust"];
+	sysimage_path="src/PkgCompile/ABCPrecompiledSysimage.so",
+	precompile_execution_file="src/PkgCompile/ABCprecompile.jl",
+	base_sysimage="src/PkgCompile/ABCSysimage.so")
 EOF
