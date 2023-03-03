815661139534d483e90c4a72b4499da143aaec28
Manuela Vanegas Ferro <44076078+manuvanegas@users.noreply.github.com>
2023-03-03 13:11:08 -0700

    Update max shade

--- src/ABM/ShadeSteps.jl
+++ src/ABM/ShadeSteps.jl
@@ -2,7 +2,7 @@ function grow_shades!(current::Books, rate::Float64)
     # for shade_i in model.current.shade_ids
     #     grow_shade!(tree, model.pars.shade_g_rate)
     # end
-    current.ind_shade += rate * (1.0 - current.ind_shade * inv(0.95)) * current.ind_shade
+    current.ind_shade += rate * (1.0 - current.ind_shade * inv(0.7)) * current.ind_shade
 end
 
 # function grow_shade!(tree::Shade, rate::Float64)
