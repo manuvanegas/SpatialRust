a2b2d1294716d7c7aaeda0918f197754cae854b6
Manuela Vanegas Ferro <44076078+manuvanegas@users.noreply.github.com>
2023-03-04 16:27:35 -0700

    Add ABC distance weighting options

--- scripts/ABC/dists/acceptRunsplotDistribs.jl
+++ scripts/ABC/dists/acceptRunsplotDistribs.jl
@@ -1,6 +1,7 @@
 using Pkg
 Pkg.activate(".")
 using Arrow, CSV, DataFrames, Random, Statistics
+using StatsBase: denserank
 using CairoMakie
 
 # violin(repeat([1,2], 10), 1:20)
@@ -12,7 +13,7 @@ include("../../../src/ABC/AcceptRuns.jl")
 include("../../../src/ABC/DistributionPlots.jl")
 include("../../../src/ABC/CustomRainclouds.jl")
 
-nmissings_max = 5
+nmissings_max = 10
 nanpenalty = 100.0
 
 parameters = DataFrame(Arrow.Table(string("data/ABC/", "parameters_", 4, ".arrow")))
@@ -22,27 +23,35 @@ scaledparams = scale_params(parameters, prior_medians)
 # relev_mets2 = metric_combination([9,10])
 qual_mets = metric_combination([5:8;])
 all_mets = metric_combination([1:8;])
+all_gmets = gmetric_combination([1:8;])
 notspore_mets = metric_combination([1; 3:8])
 noareas_mets = metric_combination([3:8;])
 
 dists = CSV.read("results/ABC/dists/squareddists.csv", DataFrame)
 ndists = CSV.read("results/ABC/dists/nmissings.csv", DataFrame)
-rmdists = rm_toomanymissings(dists, ndists, nmissings_max)
+
+rmdists = rm_toomanymissings(dists, ndists, nmissings_max, 5)
 nonansdists = replacenans(rmdists, r"prod_clr", nanpenalty)
 
 # trows = [641970,350963]
 # filter(:p_row => p -> p in trows, nonansdists)
-# filter(:RowN => p -> p in trows, parameters)
+# filter(:p_row => p -> p in trows, parameters)
 
 sel_rows = best_100(nonansdists, true, all_mets...)
+ranksel_rows = best_100ranked(nonansdists, all_mets...)
+hiersel_rows = best_100hierar(nonansdists, all_mets...)
 
 selected = get_best_params(scaledparams, sel_rows)
+rankselected = get_best_params(scaledparams, ranksel_rows)
+hierselected = get_best_params(scaledparams, hiersel_rows)
 
-write_accept_reject_runs(parameters, sel_rows, "all", nmissings_max)
+write_accept_reject_runs(parameters, sel_rows, "lowexh", nmissings_max)
+write_accept_reject_runs(parameters, ranksel_rows, "rankedquants", nmissings_max)
+write_accept_reject_runs(parameters, hiersel_rows, "newhierarquants", nmissings_max)
 # selparams = get_best_params(parameters, sel_rows)
 # selhead = first(selparams, 10)
-# append!(selhead, combine(selparams, :RowN => first, Not(:RowN) .=> median, renamecols = false))
-# selhead[11, :RowN] = -1
+# append!(selhead, combine(selparams, :p_row => first, Not(:p_row) .=> median, renamecols = false))
+# selhead[11, :p_row] = -1
 # CSV.write("results/ABC/params/selected.csv", selhead)
 
 
@@ -50,7 +59,9 @@ randpars100 = rand(1:10^6, 100)
 randpars1000 = rand(1:10^6, 1000)
 randpars1e5 = rand(1:10^6, 1*10^5)
 
-fig1 = dodged_rainclouds(scaledparams, selected, 2, randpars100, 10)
+fig1 = dodged_rainclouds(scaledparams, selected, 2, randpars1000, 100)
+fig1 = dodged_rainclouds(scaledparams, rankselected, 2, randpars1000, 100)
+fig1 = dodged_rainclouds(scaledparams, hierselected, 2, randpars1000, 100)
 fig12 = dodged_rainclouds(scaledparams, selected, 2, 1000, height = 1200)
 fig14 = dodged_rainclouds(scaledparams, selected, 2, 1000, height = 1400)
 
@@ -64,3 +75,11 @@ save("plots/ABC/all_stats1200.svg", fig12)
 save("plots/ABC/all_stats1400.svg", fig14)
 
 GC.gc()
+
+
+# ## Each quant separately
+# area100 = best_sepvars(nonansdists, :area_d, 100)
+# spore100 = best_sepvars(nonansdists, :spore_d, 100)
+# nl100 = best_sepvars(nonansdists, :nl_d, 100)
+# occup100 = best_sepvars(nonansdists, :occup_d, 100)
+# intersect(hiersel_rows.p_row, area100.p_row)
--- src/ABC/AcceptRuns.jl
+++ src/ABC/AcceptRuns.jl
@@ -1,6 +1,6 @@
 function scale_params(params::DataFrame, medians::DataFrame)
     df = similar(params)
-    df[!, :RowN] .= params[:, :RowN]
+    df[!, :p_row] .= params[:, :p_row]
     for c in 2:ncol(params)
         df[!, c] .= params[!, c] ./ medians[1, c]
     end
@@ -9,6 +9,17 @@ end
 
 function metric_combination(idx::Vector{Int})
     quants = [:area_d, :spore_d, :nl_d, :occup_d]
+    quals = [
+        :exh_sun, :prod_clr_sun, :exh_shade, :prod_clr_shade,
+        :exh_spct, :prod_clr_cor
+    ]
+    qntsid = filter(i -> i < 5, idx)
+    qlsid = filter(i -> i > 4, idx) .- 4
+    return quals[qlsid], quants[qntsid]
+end
+
+function gmetric_combination(idx::Vector{Int})
+    quants = [:area_gd, :spore_gd, :nl_gd, :occup_gd]
     quals = [
         :exh_sun, :prod_clr_sun, :exh_shade, :prod_clr_shade,
         :exh_spct, :prod_clr_cor
@@ -37,10 +48,12 @@ function metric_combination(quantop::Symbol, qualop::Symbol)
     return [quants; quals]
 end
 
-function rm_toomanymissings(dists::DataFrame, ns::DataFrame, cut::Int)
+function rm_toomanymissings(dists::DataFrame, ns::DataFrame, cut::Int, extranl::Int)
     anymorethan(nmis::Vararg{Int}) = any(n > cut for n in nmis)
-    df = transform(ns, 2:5 => ByRow(anymorethan) => :sel)
-    subset!(df, :sel)
+    anymorethannl(nmis::Vararg{Int}) = any(n > (cut + extranl) for n in nmis)
+    df = transform(ns, Not([:p_row, :nl_n]) => ByRow(anymorethan) => :sel)
+    transform!(df, :nl_n => ByRow(anymorethannl) => :selnl)
+    subset!(df, [:sel, :selnl] => max)
     return antijoin(dists, df, on = :p_row)
 end
 
@@ -52,10 +65,10 @@ function replacenans(df::DataFrame, regex::Regex, val::Float64)
 end
 
 function best_100(dists::DataFrame, qualsfirst::Bool, qualmetrics::Vector{Symbol}, quantmetrics::Vector{Symbol})
-    return best_n(dists, qualsfirst, qualmetrics, quantmetrics, 100)
+    return best_n(dists, qualsfirst, 100, qualmetrics, quantmetrics)
 end
 
-function best_n(dists::DataFrame, qualsfirst::Bool, qualmetrics::Vector{Symbol}, quantmetrics::Vector{Symbol}, n::Int)
+function best_n(dists::DataFrame, qualsfirst::Bool, n::Int, qualmetrics::Vector{Symbol}, quantmetrics::Vector{Symbol})
     if isempty(quantmetrics)
         d1 = transform(
         dists, :p_row,
@@ -86,24 +99,64 @@ function best_n(dists::DataFrame, qualsfirst::Bool, qualmetrics::Vector{Symbol},
     return d1[1:n, :]
 end
 
-get_best_params(params::DataFrame, sel_rows::DataFrame) = subset(params, :RowN => x -> x .∈ Ref(sel_rows.p_row))
+function best_100ranked(dists::DataFrame,qualmetrics::Vector{Symbol}, quantmetrics::Vector{Symbol})
+    best_ranked(dists, 100, qualmetrics, quantmetrics)
+end
+
+function best_ranked(dists::DataFrame, n::Int, qualmetrics::Vector{Symbol}, quantmetrics::Vector{Symbol})
+    d1 = transform(
+        dists, :p_row,
+        AsTable(qualmetrics) => ByRow(sqrt ∘ sum) => :qual_dist,
+        quantmetrics .=> denserank
+    )
+    select!(
+        d1, names(dists), :qual_dist,
+        AsTable(r"denserank") => ByRow(sum) => :quant_ranksum
+    )
+    sort!(d1, [:qual_dist, :quant_ranksum])
+    return d1[1:n, :]
+end
+
+best_100hierar(dists::DataFrame, qualmetrics::Vector{Symbol}, quantmetrics::Vector{Symbol}) = best_hierar(dists, 100, qualmetrics, quantmetrics)
+
+function best_hierar(dists::DataFrame, n::Int, qualmetrics::Vector{Symbol}, quantmetrics::Vector{Symbol})
+    d1 = transform(
+        dists, :p_row,
+        AsTable(qualmetrics) => ByRow(sqrt ∘ sum) => :qual_dist,
+        AsTable(quantmetrics[1:3]) => ByRow(sum) => :area_dist,
+        # AsTable(quantmetrics[1:2]) => ByRow(sum) => :area_dist,
+        # AsTable(quantmetrics[3:4]) => ByRow(sum) => :les_dist
+    )
+    sort!(d1, [:qual_dist, qualmetrics[4], :area_dist])
+    # sort!(d1, [:qual_dist, :les_dist, :area_dist])
+    return d1[1:n, :]
+end
+
+get_best_params(params::DataFrame, sel_rows::DataFrame) = subset(params, :p_row => x -> x .∈ Ref(sel_rows.p_row))
 
 function sample_rejected(sel_rows::DataFrame)
     rows = sel_rows.p_row
     rand(filter(n -> n ∉ rows, 1:10^6), 100)
 end
 
-get_rejected(params::DataFrame, sampled::Vector{Int}) = subset(params, :RowN => x -> x .∈ Ref(sampled))
+get_rejected(params::DataFrame, sampled::Vector{Int}) = subset(params, :p_row => x -> x .∈ Ref(sampled))
 
 function write_accept_reject_runs(parameters::DataFrame, sel_rows::DataFrame, metrics::String, nmissing::Int)
     selparams = get_best_params(parameters, sel_rows)
     # # selhead = first(selparams, 10)
     # append!(selhead, )
-    # selhead[11, :RowN] = -1
-    pointestimate = combine(selparams, Not(:RowN) .=> median, renamecols = false)
+    # selhead[11, :p_row] = -1
+    pointestimate = combine(selparams, Not(:p_row) .=> median, renamecols = false)
     rejected = get_rejected(parameters, sample_rejected(sel_rows))
 
     CSV.write(string("results/ABC/params/pointestimate_", metrics, "_", nmissing, ".csv"), pointestimate)
     CSV.write(string("results/ABC/params/accepted_", metrics, "_", nmissing, ".csv"), selparams)
     CSV.write(string("results/ABC/params/rejected_", metrics, "_", nmissing, ".csv"), rejected)
+end
+
+function best_sepvars(dists::DataFrame, var::Symbol, n::Int)
+    d1 = transform(
+        dists,
+        AsTable([:exh_sun, :prod_clr_sun, :exh_shade, :prod_clr_shade]) => ByRow(sqrt ∘ sum) => :qual_dist)
+    sort(d1, [:qual_dist, var])[1:n, :]
 end
