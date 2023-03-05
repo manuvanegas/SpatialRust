81a3fc7604b9ac401b8d584ec0250c7072dc4170
Manuela Vanegas Ferro <mvanega1@asu.edu>
2023-03-05 16:23:50 -0700

    Update PkgCompiler precompile file

--- src/PkgCompile/ABCprecompile.jl
+++ src/PkgCompile/ABCprecompile.jl
@@ -2,19 +2,23 @@ using DataFrames, Arrow
 using SpatialRust
 
 tdf = DataFrame(
-    p_row = [1,2], 
-    max_inf = [0.5,0.6], 
-    host_spo_inh = [0.5,1.5], 
-    opt_g_temp = [22.0, 23.0], 
-    max_g_temp = [28.0, 31.0], 
-    spore_pct = [0.5, 0.8], 
-    rust_paras = [0.1, 0.01], 
-    exh_threshold = [0.5,0.1], 
-    rain_distance = [1.0, 2.0], 
-    tree_block = [0.6, 0.9], 
-    wind_distance = [2.0, 5.0], 
-    shade_block = [0.3,0.5], 
-    lesion_survive =[0.2, 0.5]
+    p_row = [1,2, 3], 
+    max_inf = [0.5,0.4, 0.044], 
+    host_spo_inh = [5.0, 4.0, 13.11], 
+    opt_g_temp = [23.0, 23.0, 21.93], 
+    max_g_temp = [31.0, 31.0, 29.74], 
+    spore_pct = [0.5, 0.5, 0.875], 
+    rust_paras = [0.05, 0.3, 0.691], 
+    exh_threshold = [0.01,1.5, 1.211], 
+    rain_distance = [5.0, 5.0, 9.282], 
+    tree_block = [0.5, 0.5, 0.291], 
+    wind_distance = [15.0, 15.0, 11.595], 
+    shade_block = [0.5,0.5, 0.316], 
+    lesion_survive =[0.5, 0.5, 0.533],
+    rust_gr = [0.15, 0.9, 0.179],
+    rep_gro = [2.0, 3.0, 1.062],
+    shade_g_rate = [0.008, 0.008, 0.008],
+    target_shade = [0.15, 0.15, 0.15]
 )
 
 when_rust = Vector(Arrow.Table("data/exp_pro/input/whentocollect.arrow")[1])
