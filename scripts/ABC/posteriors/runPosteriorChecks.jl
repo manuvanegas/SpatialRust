c2454453c4cd462ff00460ae0056433b8efd2140
Manuela Vanegas Ferro <44076078+manuvanegas@users.noreply.github.com>
2023-03-06 21:42:00 -0700

    Update ABC posterior check runs script

--- scripts/ABC/posteriors/runPosteriorChecks.jl
+++ scripts/ABC/posteriors/runPosteriorChecks.jl
@@ -4,15 +4,14 @@
 end
 @everywhere begin
     using Arrow, CSV, DataFrames, Statistics, SpatialRust
-    include("../../../src/ABC/PosteriorChecks.jl")
+    include("../../../src/ReadWrite.jl")
 end
 
-metrics = "sentinels"
-max_missings = 5
-pardir = "results/ABC/params/"
-pdir = mkpath("results/ABC/posteriors/b")
-csvtail = string("_", metrics, "_", max_missings, ".csv")
-arrtail = string("_", metrics, "_", max_missings, ".arrow")
+metrics = "nocor"
+files = string("results/ABC/params/sents/novar/", metrics, "_")
+pdir = mkpath("results/ABC/posteriors/sents/novar/")
+csvtail = string("_", metrics, ".csv")
+arrtail = string("_", metrics, ".arrow")
 
 
 when_rust = Vector(Arrow.Table("data/exp_pro/input/whentocollect.arrow")[1])
@@ -24,22 +23,25 @@ rain_data = Tuple(w_table[3])
 wind_data = Tuple(w_table[4])
 
 accepted, rejected, pointestimate = read(
-    string.(pardir, ("accepted", "rejected", "pointestimate"), csvtail)...
+    string.(files, ("accepted", "rejected", "pointestimate"), ".csv")...
 )
 repeat!(pointestimate, 100)
-pointestimate.RowN = collect(10^6 + 1:10^6 + 100)
-select!(pointestimate, 15, 1:14)
+insertcols!(pointestimate, 1, :p_row => collect(10^6 + 1:10^6 + 100))
 
+wp = CachingPool(workers())
 acc_outs = pmap(
     p -> sim_abc(p, temp_data, rain_data, wind_data, when_2017, when_2018),
+    wp,
     eachrow(accepted); retry_delays = fill(0.1, 3)
 )
 rej_outs = pmap(
     p -> sim_abc(p, temp_data, rain_data, wind_data, when_2017, when_2018),
+    wp,
     eachrow(rejected); retry_delays = fill(0.1, 3)
 )
 point_outs = pmap(
     p -> sim_abc(p, temp_data, rain_data, wind_data, when_2017, when_2018),
+    wp,
     eachrow(pointestimate); retry_delays = fill(0.1, 3)
 )
 
@@ -50,7 +52,7 @@ point_quant, point_qual = reduce(cat_dfs, point_outs)
 quants = vcat(acc_quant, rej_quant, point_quant, source = :source => [:acc, :rej, :point])
 quals = vcat(acc_qual, rej_qual, point_qual, source = :source => [:acc, :rej, :point])
 
-cwrite(pdir, csvtail, [quants, quals], ["quant","qual"])
+writefiles(pdir, csvtail, [quants, quals], ["quant","qual"])
 
     
 
--- src/ReadWrite.jl
+++ src/ReadWrite.jl
@@ -13,3 +13,7 @@ function cwrite(path::String, tail::String, dfs::Vector{DataFrame}, names::Vecto
         CSV.write(joinpath(path, string(name, tail)), df)
     end
 end
+
+function writefiles(path::String, tail::String, dfs::Vector{DataFrame}, names::Vector{String})
+    occursin(".csv", tail) ? cwrite(path, tail, dfs, names) : awrite(path, tail, dfs, names)
+end
