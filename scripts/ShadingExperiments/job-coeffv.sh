f78caef0b75cd8d30b57883ca0189b0cd9326c0e
Manuela Vanegas Ferro <44076078+manuvanegas@users.noreply.github.com>
2023-03-08 16:49:23 -0700

    Update Coefficient of Variance runs

--- scripts/ShadingExperiments/job-coeffv.sh
+++ scripts/ShadingExperiments/job-coeffv.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#SBATCH --ntasks=20
+#SBATCH --ntasks=5
 #SBATCH --ntasks-per-core=1
 
-#SBATCH -p debug
-#SBATCH -q wildfire
+#SBATCH -p htc
+#SBATCH -q debug
 #SBATCH -J debug-CVs
 #SBATCH -t 0-00:15:00
-
 #SBATCH -o logs/shading/o-%A.o
 #SBATCH -e logs/shading/o-%A.e
 #SBATCH --mail-type=ALL
@@ -14,8 +13,9 @@
 
 
 module purge
-module load julia/1.7.2
+module load julia/1.8.2
 
-export SLURM_NODEFILE=`generate_pbs_nodefile`
+export SLURM_NODEFILE=`scripts/generate_pbs_nodefile.pl`
 julia --machine-file $SLURM_NODEFILE \
-~/SpatialRust/scripts/ShadingExperiments/CoeffVariance.jl 1003 22.5 0.8
+~/SpatialRust/scripts/ShadingExperiments/CoeffVariance.jl 100 23.0 0.8
+# 1003
--- src/ShadingExperiments/NumberOfRuns.jl
+++ src/ShadingExperiments/NumberOfRuns.jl
@@ -1,16 +1,14 @@
 ## Functions to run parameter space exploration for Chapter 1
 
-function coeff_vars(n::Int, mtemp::Float64, rainp::Float64)
+function coeff_vars(n::Int, mtemp::Float64, rainp::Float64,
+    pars::DataFrame = DataFrame())
     ns = vcat(collect(25:25:75), collect(100:100:600), 800, 1000)
-    #if n == 100
-    #    ns = collect(20:20:100)
-    #end
+    if n == 100
+       ns = collect(20:20:100)
+    end
     a_ns = filter(x -> x .<= n, ns)
     # coeff_vars = DataFrame(n = Int[], prod = Float64[], area = Float64[])
-
-    run_ns = reduce(vcat, fill.(a_ns, a_ns))
-
-    df = cv_n_sims(run_ns, mtemp, rainp)
+    df = cv_n_sims(a_ns, pars, mtemp, rainp)
 
     coeff_vars = combine(groupby(df, :n), [:totprod, :maxA] =>
         ((p, a) -> (prod = (std(p) / mean(p)),
@@ -31,34 +29,21 @@ function coeff_vars(n::Int, mtemp::Float64, rainp::Float64)
     return coeff_vars
 end
 
-function cv_n_sims(run_ns::Vector{Int}, mtemp::Float64, rainp::Float64)::DataFrame
-    pars = Parameters(
-        steps = 1460,
-        prune_period = 365,
-        target_shade = 0.4,
-        shade_d = 10,
-        barriers = (0,0),
-        rain_prob = rainp,
-        mean_temp = mtemp,
-        # from ABC
-        rust_gr = 1.63738,
-        cof_gr = 0.393961,
-        spore_pct = 0.821479,
-        fruit_load = 0.597133,
-        light_inh = 0.166768,
-        rain_washoff = 0.23116,
-        rain_distance = 0.80621,
-        wind_distance = 3.29275,
-        exhaustion = 0.17458)
+function cv_n_sims(a_ns::Vector{Int}, mtemp::Float64, rainp::Float64, pars::DataFrame)::DataFrame
+    run_ns = reduce(vcat, fill.(a_ns, a_ns))
+    fmap = create_farm_map(100, 2, 1, 9, :regular, 1, (0,0))
 
-    fmap = SpatialRust.create_farm_map(pars)
+    if isempty(pars)
+        pars = CSV.read("results/ABC/params/sents/novar/byaroccexhincid_pointestimate.csv", DataFrame)
+    end
 
     # df = DataFrame(run = Int[], totprod = Float64[], maxA = Float64[])
     # for 1:n
     #     push!(df, one_cv_sim(pars, map))
     # end
 
-    dfs = pmap(x -> one_cv_sim(pars, fmap, x), run_ns)
+    wp = CachingPool(workers())
+    dfs = pmap(x -> one_cv_sim(fmap, abcpars, mtemp, rainp, x), wp, run_ns)
 
     df = reduce(vcat, dfs)
 
@@ -71,16 +56,35 @@ function cv_n_sims(run_ns::Vector{Int}, mtemp::Float64, rainp::Float64)::DataFra
     return df
 end
 
-# function wrapper_one_cv(pars::Parameters, farm_map::Array{Int,2}, x::Int)::DataFrame
-#     return one_cv_sim(pars, farm_map)
-# end
+function one_cv_sim(fmap::Array{Int,2}, pars::DataFrame, mtemp::Float64, rainp::Float64, n::Int)::DataFrame
+    ss = 1461
+    model = init_spatialrust(
+        steps = ss,
+        prune_sch = [182,-1,-1], 
+        target_shade = 0.1,
+        farm_map = fmap,
+        rain_prob = rainp,
+        mean_temp = mtemp,
+        # from ABC
+        pars[1,:]...)
 
-function one_cv_sim(pars::Parameters, farm_map::Array{Int,2}, n::Int)::DataFrame
-    # one_sim for par has to create its own farm_map each time
-    model = init_spatialrust(pars, farm_map)
-    _ , mdf = run!(model, dummystep, step_model!, pars.steps;
-        when_model = [pars.steps],
-        mdata = [totprod, maxA])
-    mdf[:, :n] .= n
-    return mdf[:, [:totprod, :maxA, :n]]
+    return custom_run!(model, ss, n)
+end
+
+function custom_run!(model::SpatialRustABM, steps::Int, n::Int)
+    max_area = 0
+
+    s = 0
+    while Agents.until(s, steps, model)
+        step!(model, dummystep, step_model!, 1)
+        sumareas = filter(>(0.0), sum.(getproperty.(model.agents, :area)))
+        if !isempty(sumareas)
+            if (currentarea = mean(sumareas)) > max_area
+                max_area = currentarea
+            end
+        end
+        s += 1
+    end
+
+    return DataFrame(totprod = model.current.prod, maxA = max_area, n = n)
 end
