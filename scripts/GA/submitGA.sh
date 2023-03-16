860143759284d156a22be75026d679878df7d871
Manuela Vanegas Ferro <44076078+manuvanegas@users.noreply.github.com>
2023-03-15 22:38:52 -0700

    Create submitGA.sh

---
+++ scripts/GA/submitGA.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --ntasks=5
+#SBATCH --ntasks-per-core=1
+# #SBATCH --mem=5G
+
+#SBATCH -p htc
+#SBATCH -q debug
+#SBATCH -t 15:00
+#SBATCH -J debugGA
+# #SBATCH -q public
+# #SBATCH -J GA
+# #SBATCH -t 0-04:00:00
+#SBATCH -o logs/ga/%x-%A.o
+#SBATCH -e logs/ga/%x-%A.e
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mvanega1@asu.edu
+
+
+module purge
+module load julia/1.8.2
+
+export SLURM_NODEFILE=`scripts/generate_pbs_nodefile.pl`
+julia --machine-file $SLURM_NODEFILE \
+~/SpatialRust/scripts/GA/runGA.jl 100 20 10 0.7 0.1 365 1.0 shortprofit
+# popsize, gens, reps, p cross, p mut, steps, coffeeprice, obj
