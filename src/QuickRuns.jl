25cf031ba0720839dc174ff3daf6722026ae9a18
Manuela Vanegas Ferro <44076078+manuvanegas@users.noreply.github.com>
2023-03-16 00:04:37 -0700

    Create `simplerun`

--- src/QuickRuns.jl
+++ src/QuickRuns.jl
@@ -1,4 +1,4 @@
-export dummyrun_spatialrust, dummyrun_fullsun_spatialrust, justtwosteps
+export dummyrun_spatialrust, simplerun, justtwosteps
 
 function dummyrun_spatialrust(steps::Int = 200, side::Int = 60, maxlesions::Int = 25; kwargs...)
     model = init_spatialrust(steps = steps, map_side = side, max_lesions = maxlesions; kwargs...)
@@ -28,4 +28,83 @@ function justtwosteps(side::Int = 60)
     model = init_spatialrust(steps = steps, map_side = side, max_lesions = maxlesions, common_map = :fullsun)
     step!(model, dummystep, step_model!, 2)
     return model
+end
+
+function simplerun(steps::Int = 365; kwargs...)
+    model = init_spatialrust(steps = steps; kwargs...)
+
+    df = runsimple!(model, steps)
+
+    return df
+end
+
+function runsimple!(model::SpatialRustABM, steps::Int)
+    meanshade = mean(model.shade_map)
+    ncofs = length(model.agents)
+
+    df = DataFrame(dayn = Int[],
+        veg = Float64[], storage = Float64[], production = Float64[],
+        indshade = Float64[], mapshade = Float64[],
+        nl = Float64[], sumarea = Float64[], sumspore = Float64[],
+        active = Float64[]
+    )
+    for c in eachcol(df)
+        sizehint!(c, steps)
+    end
+
+    s = 0
+    while s < steps
+        indshade = model.current.ind_shade
+
+        sumareas = filter(>(0.0), sum.(getproperty.(model.agents, :areas)))
+        if isempty(sumareas)
+            msuma = 0.0
+            msumsp = 0.0
+        else
+            msuma = mean(sumareas)
+            sumspores = filter(>(0.0), sum.(getproperty.(model.agents, :spores)))
+            isempty(sumareas) ? (msumsp = 0.0) : (msumsp = mean(sumspores))
+        end
+
+        push!(df, [
+            s,
+            mean(getproperty.(model.agents, :veg)),
+            mean(getproperty.(model.agents, :storage)),
+            mean(getproperty.(model.agents, :production)),
+            indshade,
+            indshade * meanshade,
+            mean(getproperty.(model.agents, :n_lesions)),
+            msuma,
+            msumsp,
+        sum(active.(model.agents)) / ncofs
+        ])
+        step!(model, dummystep, step_model!, 1)
+        s += 1
+    end
+
+    indshade = model.current.ind_shade
+
+    sumareas = filter(>(0.0), sum.(getproperty.(model.agents, :areas)))
+    if isempty(sumareas)
+        msuma = 0.0
+        msumsp = 0.0
+    else
+        msuma = mean(sumareas)
+        sumspores = filter(>(0.0), sum.(getproperty.(model.agents, :spores)))
+        isempty(sumareas) ? (msumsp = 0.0) : (msumsp = mean(sumspores))
+    end
+
+    push!(df, [
+        s,
+        mean(getproperty.(model.agents, :veg)),
+        mean(getproperty.(model.agents, :storage)),
+        mean(getproperty.(model.agents, :production)),
+        indshade,
+        indshade * meanshade,
+        mean(getproperty.(model.agents, :n_lesions)),
+        msuma,
+        msumsp,
+    sum(active.(model.agents)) / ncofs
+    ])
+    return df
 end
