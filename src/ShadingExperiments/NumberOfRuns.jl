f8eaf8c1189b368b3a6fb440fe5d9a5cc85374e6
Manuela Vanegas Ferro <44076078+manuvanegas@users.noreply.github.com>
2023-04-06 12:27:55 -0700

    Update CoeffVar: area -> spore + add incidence

--- src/ShadingExperiments/NumberOfRuns.jl
+++ src/ShadingExperiments/NumberOfRuns.jl
@@ -8,26 +8,15 @@ function coeff_vars(n::Int, mtemp::Float64, rainp::Float64,
     #    ns = collect(20:20:100)
     # end
     a_ns = filter(x -> x .<= n, ns)
-    # coeff_vars = DataFrame(n = Int[], prod = Float64[], area = Float64[])
+
     df = cv_n_sims(a_ns, pars, mtemp, rainp)
 
-    coeff_vars = combine(groupby(df, :n), [:totprod, :maxA] =>
-        ((p, a) -> (
+    coeff_vars = combine(groupby(df, :n), [:totprod, :maxS, :maxI] =>
+        ((p, s, inc) -> (
             prod = (std(p) / mean(p)),
-            area = (std(a) / mean(a)))
-            # corrprod = (std(log10.(p)) / mean(log10.(p))))
+            spore = (std(s) / mean(s)),
+            incid = (std(inc) / mean(inc)))
         ) => AsTable)
-
-    # for run in run_ns
-# "should try sequential instead of samples"
-        # df = cv_n_sims(n, mtemp, rainp)
-        # rows = sample(1:n, samples, replace = false)
-        # thisdf = df[rows, :]
-        # sds_means = combine(thisdf, All() .=> std, All() .=> mean)
-        # push!(coeff_vars, (samples, (sds_means[1, :totprod_std] / sds_means[1, :totprod_mean]),
-        # (sds_means[1, :maxA_std] / sds_means[1, :maxA_mean])))
-        # append!(coeff_vars, sds_means)
-    # end
 
     return coeff_vars
 end
@@ -37,7 +26,7 @@ function cv_n_sims(a_ns::Vector{Int}, pars::DataFrame, mtemp::Float64, rainp::Fl
     fmap = create_farm_map(100, 2, 1, 9, :regular, 1, (0,0))
 
     if isempty(pars)
-        pars = CSV.read("results/ABC/params/sents/novar/byaroccincid_pointestimate.csv", DataFrame)
+        pars = CSV.read("results/ABC/params/sents/q8/byoccnl_pointestimate.csv", DataFrame)
     end
 
     # df = DataFrame(run = Int[], totprod = Float64[], maxA = Float64[])
@@ -68,10 +57,10 @@ function one_cv_sim(fmap::Array{Int,2}, pars::DataFrame, mtemp::Float64, rainp::
     ss = 1461
     model = init_spatialrust(
         steps = ss,
-        inspect_period = 460,
+        inspect_period = ss,
         fungicide_sch = Int[],
         prune_sch = [182,-1,-1], 
-        target_shade = 0.1,
+        target_shade = [0.15, -1, -1],
         shade_g_rate = 0.008,
         farm_map = copy(fmap),
         rain_prob = rainp,
@@ -83,19 +72,31 @@ function one_cv_sim(fmap::Array{Int,2}, pars::DataFrame, mtemp::Float64, rainp::
 end
 
 function custom_run!(model::SpatialRustABM, steps::Int, n::Int)
-    max_area = 0
+    # pre_run!(model)
+    allcofs = model.agents
+    maxspores = 0.0
+    maxinf = 0.0
 
     s = 0
-    while Agents.until(s, steps, model)
-        step!(model, dummystep, step_model!, 1)
-        sumareas = filter(>(0.0), sum.(getproperty.(model.agents, :areas)))
-        if !isempty(sumareas)
-            if (currentarea = mean(sumareas)) > max_area
-                max_area = currentarea
+    myaupcS = 0.0
+    myaupcI = 0.0
+    while s < steps
+        step_model!(model)
+        myaupcS += sum(map(r -> mean(r.spores), allcofs))
+        myaupcI += mean(map(r -> r.n_lesions > 0, allcofs))
+        if s % 365 == 0
+            if myaupcS > maxspores
+                maxspores = myaupcS
             end
+            if myaupcI > maxinf
+                maxinf = myaupcI
+            end
+            myaupcS = 0.0
+            myaupcI = 0.0
         end
         s += 1
     end
 
-    return DataFrame(totprod = model.current.prod, maxA = max_area, n = n)
+    return DataFrame(totprod = model.current.prod, maxS = maxspores, maxI = maxinf, n = n)
 end
+
