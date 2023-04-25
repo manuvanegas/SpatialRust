0c6643dd20eaff5f5396c5ef009a915a841f05c0
Manuela Vanegas Ferro <44076078+manuvanegas@users.noreply.github.com>
2023-04-25 15:54:50 -0700

    Update NoVarDists

--- scripts/ABC/dists/calcDistsnoVar.jl
+++ scripts/ABC/dists/calcDistsnoVar.jl
@@ -11,44 +11,56 @@ end
 # ARGS: quantsdirname, qualsdirname
 quantsdirname = ARGS[1]
 qualsdirname = ARGS[2]
-qldats = DataFrame(
-    P1att = [-0.005, 0.6], #
-    bienniality = [-0.01, 1.0],
-    Pattpct = [0.0, 0.25],
-    areas = [-0.1, 25.0],
-    nls = [-25.0, 0.1],
-    P1loss = [0.1, 0.65], ##
-    P12loss = [1.1, 0.95], # ##
-    incidiff = [0.35, 1.0],
-    cor = [0.2, 0.95]
+qldats = (
+    P12loss = [0.3, 0.85], #
+    LP = [25, 65],
+    incid = [0.7, 1.01],
+    rusts = [10, 5001],
+    meandeps = [-10.0, 0.001],
+    meanlatent = [-0.001, 10.0], ##
+    cor = [0.2, 0.95], # ##
 )
+# qldats = DataFrame(
+#     P1att = [-0.005, 0.6], #
+#     bienniality = [-0.01, 1.0],
+#     Pattpct = [0.0, 0.25],
+#     areas = [-0.1, 25.0],
+#     nls = [-25.0, 0.1],
+#     P1loss = [0.1, 0.65], ##
+#     P12loss = [1.1, 0.95], # ##
+#     incidiff = [0.35, 1.0],
+#     cor = [0.2, 0.95]
+# )
 
-mkpath("results/ABC/dists/sents/q9")
+v = "10a"
 
-empdata = DataFrame(Arrow.Table("data/exp_pro/compare/perdate_age_long_10.arrow"))
-qntvars = CSV.read("results/ABC/variances/sents/q9/v_gquants.csv", DataFrame)
-qlvars = CSV.read("results/ABC/variances/sents/q9/v_quals.csv", DataFrame)
+mkpath(string("results/ABC/dists/sents/", v))
 
+empdata = DataFrame(Arrow.Table("data/exp_pro/v2/compare.arrow"))
+# qntvars = CSV.read("results/ABC/variances/sents/q9/v_gquants.csv", DataFrame)
+# qlvars = CSV.read("results/ABC/variances/sents/q9/v_quals.csv", DataFrame)
 
 time_dists = @elapsed begin
-    l_distsn, l_distsv = calc_l_dists(qualsdirname, qldats, qlvars[1,:])
-    nt_dists, nmissings = calc_nt_dists(quantsdirname, empdata, qntvars[1,:])
+    l_dists = calc_l_dists(qualsdirname, qldats)
+    nt_dists, nmissings = calc_nt_dists(quantsdirname, empdata)
+    # l_distsn, l_distsv = calc_l_dists(qualsdirname, qldats, qlvars[1,:])
+    # nt_dists, nmissings = calc_nt_dists(quantsdirname, empdata, qntvars[1,:])
 end
 println("Distances: $time_dists")
 flush(stdout)
 
 time_joinwrite = @elapsed begin
     # count # observations per stat
-    obscounts = count_obs(empdata)
+    # obscounts = count_obs(empdata)
     # scale quant dists by # obs
-    scale_dists!(nt_dists, obscounts)
+    # scale_dists!(nt_dists, obscounts)
     # join quant and qual dists
-    ndists = leftjoin(nt_dists, l_distsn, on = :p_row)
-    vdists = leftjoin(nt_dists, l_distsv, on = :p_row)
+    ndists = leftjoin(nt_dists, l_dists, on = :p_row)
+    # vdists = leftjoin(nt_dists, l_distsv, on = :p_row)
     # write
     # CSV.write("results/ABC/dists/sents/novar/squareddists.csv", dists)
-    CSV.write("results/ABC/dists/sents/q9/squareddists_n.csv", ndists)
-    CSV.write("results/ABC/dists/sents/q9/squareddists_v.csv", vdists)
-    CSV.write("results/ABC/dists/sents/q9/nmissings.csv", nmissings)
+    CSV.write(string("results/ABC/dists/sents/", v, "/squareddists.csv"), ndists)
+    # CSV.write(string("results/ABC/dists/sents/", v, "/squareddists_v.csv"), vdists)
+    CSV.write(string("results/ABC/dists/sents/", v, "/nmissings.csv"), nmissings)
 end
 println("Join+write: $time_joinwrite")
--- src/ABC/NoVarDistances.jl
+++ src/ABC/NoVarDistances.jl
@@ -1,12 +1,14 @@
 # Calculate distances using FileTrees
 
 ## "Qualitative" variables
-function calc_l_dists(qualsdirname::String, dats::DataFrame, vars::DataFrameRow)::NTuple{2, DataFrame}
+# function calc_l_dists(qualsdirname::String, dats::DataFrame, vars::DataFrameRow)::NTuple{2, DataFrame}
+function calc_l_dists(qualsdirname::String, dats)::NTuple{2, DataFrame}
     ft = FileTree(string("/scratch/mvanega1/ABC/sims/", qualsdirname))
     l_file_tree = FileTrees.load(ft; lazy = true) do file
         DataFrame(Arrow.Table(path(file)))
     end
-    lazy_dists = mapvalues(x -> diff_quals(x, dats, vars), l_file_tree)
+    lazy_dists = mapvalues(x -> diff_quals(x, dats), l_file_tree)
+    # lazy_dists = mapvalues(x -> diff_quals(x, dats, vars), l_file_tree)
     rowdists = exec(reducevalues(vcat, lazy_dists))
 
     # replace!(dists_l[!, :cor_d]) do cor
@@ -18,7 +20,8 @@ function calc_l_dists(qualsdirname::String, dats::DataFrame, vars::DataFrameRow)
     #     Not(:p_row) .=> missum,
     #     renamecols = false
     # )
-    return select(rowdists, :p_row, r"_dn"), select(rowdists, :p_row, r"_dv")
+    return rowdists
+    # return select(rowdists, :p_row, r"_dn"), select(rowdists, :p_row, r"_dv")
 end
 
 # function missum(v)
@@ -33,96 +36,16 @@ end
 # end
 
 # function diff_quals(sims::DataFrame, exh_min::Float64, exh_max::Float64, incidm::Float64, corm::Float64)::DataFrame
-function diff_quals(sims::DataFrame, dats::DataFrame, vars::DataFrameRow)::DataFrame
-
-    # divide total production by number of coffees to get yield per coffee
-    prodcols = [:P1att, :P12att, :P1obs, :P12obs, :plot]
-    sims = transform(sims,
-        prodcols => ByRow(ind_yield) => prodcols
-    )
-
-    # diffmetrics: metrics whose relevance is in the difference between sun and shade
-    diffmetrics = select(sims, :p_row, :plot,
-        [:P1att, :P12att] => ByRow(bienniality) => [:P1att, :bienniality, :P12att],
-        :areas, :nls
-    )
-
-    dfsun = subset(diffmetrics, :plot => ByRow(==(:sun)), view = true)[!, Not(:plot)]
-    dfsh = subset(diffmetrics, :plot => ByRow(==(:shade)), view = true)[!, Not(:plot)]
-    widedf = outerjoin(dfsun, dfsh, on = :p_row, renamecols = "_sun" => "_sh")
-
-    for m in [:P1att, :bienniality, :areas, :nls]
-        widedf[!, m] = widedf[!, Symbol(m, :_sun)] - widedf[!, Symbol(m, :_sh)]
+function diff_quals(sims::DataFrame, dats)::DataFrame
+    dists = DataFrame(p_row = joined[:, :p_row])
+    for var in [:P12loss, :LP, :incid, :rusts]
+        # transform!(sims, var => ByRow(s -> sumtoldist(s, dats[var])), renamecols = false)
+        dists[!, var] = sumtoldist.(sims[!, var], Ref(dats[var]))
     end
-    widedf[!, :Pattpct] = 1.0 .- widedf[!, :P12att_sh] ./ widedf[!, :P12att_sun]
-
-    diffdists = DataFrame(p_row = widedf[:, :p_row])
-    for name in [:Pattpct, :bienniality, :areas, :nls]
-        diffdists[!, Symbol(name, :_dn)] = toldist.(widedf[!, name], Ref(dats[!, name]))
-        diffdists[!, Symbol(name, :_dv)] = diffdists[!, Symbol(name, :_dn)] ./ vars[name]
+    for var in [:meandeps, :meanlatent, :cor]
+        dists[!, var] = toldist.(sims[!, var], Ref(dats[var]))
     end
-    diffdists[!, :P1att_dn] = toldist.(widedf[!, :P1att], widedf[!, :P1att_sun] .* Ref(dats[!, :P1att]))
-    diffdists[!, :P1att_dv] = diffdists[!, :P1att_dn] ./ vars[:P1att]
-
-    # metricsdiff = [:P1att, :bienniality, :areas, :nls]
-
-    # dfsun[!, metricsdiff] = dfsun[!, metricsdiff] .- dfsh[!, metricsdiff]
-    # dfsun[!, :Pattpct] = 1.0 .- dfsh[!, :P12att] ./ dfsun[!, :P12att]
-
-    # diffdists = DataFrame(p_row = unique(sims[:, :p_row]))
-    # for name in [:Pattpct, :bienniality, :areas, :nls]
-    #     diffdists[!, Symbol(name, :_d)] .= toldist.(dfsun[!, name], Ref(dats[!, name]), vars[name])
-    # end
-    # diffdists[!, :P1att_d] = toldistsep.(dfsun[!, :P1att], dats[1, :P1att], dfsun[!, :P1att] .* dats[2, :P1att], vars[:P1att])
-
-
-
-    # selfmetrics: metrics that are "self-relevant"
-    # distance output is the mean of sun and shade dists
-    selfmetrics = select(sims, :p_row,
-        [:P1att, :P1obs, :P12att, :P12obs] => ByRow(yieldloss) => [:P1loss, :P12loss],
-        :incidiff, :cor, :rusts, :active
-    )
-    
-    selfmetrics[!, :P1loss_dn] = baltoldist.(selfmetrics[!, :P1loss], dats[1, :P1loss], dats[2, :P1loss], 3.5) # dist from 0.65 to 1 is 3.5X dist from 0 to 0.1
-    selfmetrics[!, :P1loss_dv] = selfmetrics[!, :P1loss_dn] ./ vars[:P1loss]
-
-    selfmetrics[!, :P12loss_dn] = baltoldist.(
-        selfmetrics[!, :P12loss],
-        minloss.(0.2, selfmetrics[!, :P1loss] .* dats[1, :P12loss]),
-        dats[2, :P12loss], 2.0) # double weight for losses that are too small
-    selfmetrics[!, :P12loss_dv] = selfmetrics[!, :P12loss_dn] ./ vars[:P12loss]
-
-    for name in [:incidiff, :cor]
-        selfmetrics[!, Symbol(name, :_dn)] = toldist.(selfmetrics[!, name], Ref(dats[!, name]))
-        selfmetrics[!, Symbol(name, :_dv)] = selfmetrics[!, Symbol(name, :_dn)] ./ vars[name]
-    end
-    # selfmetrics[!, :surv_dn] = selfmetrics[!, [:rusts]] .> 4 .&& selfmetrics[!, :active] .> 4
-    transform!(selfmetrics, [:rusts, :active] => ByRow(survival) => :surv_dn)
-    selfmetrics[!, :surv_dv] = selfmetrics[!, :surv_dn]
-    
-    prowdists = combine(groupby(selfmetrics, :p_row),
-    Cols(r"_d") .=> mean,
-    renamecols = false)
-
-
-    leftjoin!(prowdists, diffdists, on = :p_row)
-
-    return prowdists
-end
-
-function ind_yield(P1att, P12att, P1obs, P12obs, plot::Symbol)
-    ncofs = ifelse(plot == :sun, 5000, 4711)
-    return P1att / ncofs, P12att / ncofs, P1obs / ncofs, P12obs / ncofs, plot
-end
-
-function yieldloss(y1att, y1obs, y12att, y12obs)
-    # y1loss = (y1att - y1obs) / y1att
-    return ((y1att - y1obs) / y1att), ((y12att - y12obs) / y12att)
-end
-
-function bienniality(Y1, Y12)
-    return Y1, (abs(2.0 * Y1 - Y12) / Y1), Y12
+    return dists
 end
 
 function toldist(sim::Float64, tol::Vector{Float64})
@@ -140,6 +63,104 @@ end
 
 toldist(sim::Missing, tol) = 1e5
 
+sumtoldist(sim::Vector{Float64}, tol::Vector{Float64}) = toldist(sim[1], tol) + toldist(sim[2], tol)
+
+sumtoldist(sim, tol::Vector{Float64}) = 1e5
+
+# function diff_quals(sims::DataFrame, dats::DataFrame, vars::DataFrameRow)::DataFrame
+
+#     # divide total production by number of coffees to get yield per coffee
+#     prodcols = [:P1att, :P12att, :P1obs, :P12obs, :plot]
+#     sims = transform(sims,
+#         prodcols => ByRow(ind_yield) => prodcols
+#     )
+
+#     # diffmetrics: metrics whose relevance is in the difference between sun and shade
+#     diffmetrics = select(sims, :p_row, :plot,
+#         [:P1att, :P12att] => ByRow(bienniality) => [:P1att, :bienniality, :P12att],
+#         :areas, :nls
+#     )
+
+#     dfsun = subset(diffmetrics, :plot => ByRow(==(:sun)), view = true)[!, Not(:plot)]
+#     dfsh = subset(diffmetrics, :plot => ByRow(==(:shade)), view = true)[!, Not(:plot)]
+#     widedf = outerjoin(dfsun, dfsh, on = :p_row, renamecols = "_sun" => "_sh")
+
+#     for m in [:P1att, :bienniality, :areas, :nls]
+#         widedf[!, m] = widedf[!, Symbol(m, :_sun)] - widedf[!, Symbol(m, :_sh)]
+#     end
+#     widedf[!, :Pattpct] = 1.0 .- widedf[!, :P12att_sh] ./ widedf[!, :P12att_sun]
+
+#     diffdists = DataFrame(p_row = widedf[:, :p_row])
+#     for name in [:Pattpct, :bienniality, :areas, :nls]
+#         diffdists[!, Symbol(name, :_dn)] = toldist.(widedf[!, name], Ref(dats[!, name]))
+#         diffdists[!, Symbol(name, :_dv)] = diffdists[!, Symbol(name, :_dn)] ./ vars[name]
+#     end
+#     diffdists[!, :P1att_dn] = toldist.(widedf[!, :P1att], widedf[!, :P1att_sun] .* Ref(dats[!, :P1att]))
+#     diffdists[!, :P1att_dv] = diffdists[!, :P1att_dn] ./ vars[:P1att]
+
+#     # metricsdiff = [:P1att, :bienniality, :areas, :nls]
+
+#     # dfsun[!, metricsdiff] = dfsun[!, metricsdiff] .- dfsh[!, metricsdiff]
+#     # dfsun[!, :Pattpct] = 1.0 .- dfsh[!, :P12att] ./ dfsun[!, :P12att]
+
+#     # diffdists = DataFrame(p_row = unique(sims[:, :p_row]))
+#     # for name in [:Pattpct, :bienniality, :areas, :nls]
+#     #     diffdists[!, Symbol(name, :_d)] .= toldist.(dfsun[!, name], Ref(dats[!, name]), vars[name])
+#     # end
+#     # diffdists[!, :P1att_d] = toldistsep.(dfsun[!, :P1att], dats[1, :P1att], dfsun[!, :P1att] .* dats[2, :P1att], vars[:P1att])
+
+
+
+#     # selfmetrics: metrics that are "self-relevant"
+#     # distance output is the mean of sun and shade dists
+#     selfmetrics = select(sims, :p_row,
+#         [:P1att, :P1obs, :P12att, :P12obs] => ByRow(yieldloss) => [:P1loss, :P12loss],
+#         :incidiff, :cor, :rusts, :active
+#     )
+    
+#     selfmetrics[!, :P1loss_dn] = baltoldist.(selfmetrics[!, :P1loss], dats[1, :P1loss], dats[2, :P1loss], 3.5) # dist from 0.65 to 1 is 3.5X dist from 0 to 0.1
+#     selfmetrics[!, :P1loss_dv] = selfmetrics[!, :P1loss_dn] ./ vars[:P1loss]
+
+#     selfmetrics[!, :P12loss_dn] = baltoldist.(
+#         selfmetrics[!, :P12loss],
+#         minloss.(0.2, selfmetrics[!, :P1loss] .* dats[1, :P12loss]),
+#         dats[2, :P12loss], 2.0) # double weight for losses that are too small
+#     selfmetrics[!, :P12loss_dv] = selfmetrics[!, :P12loss_dn] ./ vars[:P12loss]
+
+#     for name in [:incidiff, :cor]
+#         selfmetrics[!, Symbol(name, :_dn)] = toldist.(selfmetrics[!, name], Ref(dats[!, name]))
+#         selfmetrics[!, Symbol(name, :_dv)] = selfmetrics[!, Symbol(name, :_dn)] ./ vars[name]
+#     end
+#     # selfmetrics[!, :surv_dn] = selfmetrics[!, [:rusts]] .> 4 .&& selfmetrics[!, :active] .> 4
+#     transform!(selfmetrics, [:rusts, :active] => ByRow(survival) => :surv_dn)
+#     selfmetrics[!, :surv_dv] = selfmetrics[!, :surv_dn]
+    
+#     prowdists = combine(groupby(selfmetrics, :p_row),
+#     Cols(r"_d") .=> mean,
+#     renamecols = false)
+
+
+#     leftjoin!(prowdists, diffdists, on = :p_row)
+
+#     return prowdists
+# end
+
+# function ind_yield(P1att, P12att, P1obs, P12obs, plot::Symbol)
+#     ncofs = ifelse(plot == :sun, 5000, 4711)
+#     return P1att / ncofs, P12att / ncofs, P1obs / ncofs, P12obs / ncofs, plot
+# end
+
+# function yieldloss(y1att, y1obs, y12att, y12obs)
+#     # y1loss = (y1att - y1obs) / y1att
+#     return ((y1att - y1obs) / y1att), ((y12att - y12obs) / y12att)
+# end
+
+# function bienniality(Y1, Y12)
+#     return Y1, (abs(2.0 * Y1 - Y12) / Y1), Y12
+# end
+
+
+
 # function toldistsep(sim::Union{Missing, Float64}, tmin::Union{Missing, Float64}, tmax::Union{Missing, Float64})
 #     if ismissing(sim) || !isfinite(sim)
 #         return 1e5
@@ -152,85 +173,105 @@ toldist(sim::Missing, tol) = 1e5
 #     end
 # end
 
-baltoldist(sim::Missing, tmin, tmax::Float64, bal::Float64) = 1e5
+# baltoldist(sim::Missing, tmin, tmax::Float64, bal::Float64) = 1e5
 
-function baltoldist(sim::Float64, tmin::Float64, tmax::Float64, bal::Float64)
-    if !isfinite(sim)
-        return 1e5
-    elseif sim > tmax
-        return (sim - tmax) ^ 2
-    elseif sim < tmin
-        return (bal * (tmin - sim)) ^ 2
-    else
-        return 0.0
-    end
-end
+# function baltoldist(sim::Float64, tmin::Float64, tmax::Float64, bal::Float64)
+#     if !isfinite(sim)
+#         return 1e5
+#     elseif sim > tmax
+#         return (sim - tmax) ^ 2
+#     elseif sim < tmin
+#         return (bal * (tmin - sim)) ^ 2
+#     else
+#         return 0.0
+#     end
+# end
 
-minloss(tmin::Float64, sim::Float64) = max(tmin, sim)
+# minloss(tmin::Float64, sim::Float64) = max(tmin, sim)
 
-minloss(tmin::Float64, sim::Missing) = missing
+# minloss(tmin::Float64, sim::Missing) = missing
 
-survival(rust::Int, cof::Int) = rust > 4 && cof > 4
-survival(rust::Missing, cof::Missing) = false
+# survival(rust::Int, cof::Int) = rust > 4 && cof > 4
+# survival(rust::Missing, cof::Missing) = false
 
 ## Quantitative variables
-function calc_nt_dists(quantsdirname::String, empdata::DataFrame, vars::DataFrameRow)::NTuple{2, DataFrame}
+
+function calc_nt_dists(quantsdirname::String, empdata::DataFrame)::NTuple{2, DataFrame}
+# function calc_nt_dists(quantsdirname::String, empdata::DataFrame, vars::DataFrameRow)::NTuple{2, DataFrame}
     ft = FileTree(string("/scratch/mvanega1/ABC/sims/", quantsdirname))
     nt_file_tree = FileTrees.load(ft; lazy = true) do file
         DataFrame(Arrow.Table(path(file)))
     end
-    lazy_dists = mapvalues(x -> abs_norm_dist(x, empdata, vars), nt_file_tree)
+    lazy_dists = mapvalues(x -> abs_norm_dist(x, empdata), nt_file_tree)
+    # lazy_dists = mapvalues(x -> abs_norm_dist(x, empdata, vars), nt_file_tree)
     println("starting quants exec...")
     flush(stdout)
     dists_nt = exec(reducevalues(vcat, lazy_dists))
-    return select(dists_nt, :p_row, r"_d"), select(dists_nt, :p_row, r"_n")
+    return select(dists_nt, :p_row, Not(r"_n")), select(dists_nt, :p_row, r"_n")
 end
 
-function abs_norm_dist(sims::DataFrame, empdata::DataFrame, vars::DataFrameRow)::DataFrame
-    joined = leftjoin(empdata, sims, on = [:plot, :dayn, :age, :cycle])
-
-    any(ismissing.(joined.p_row)) && error("missing rows $(first(skipmissing(joined.p_row)))")
+function abs_norm_dist(sims::DataFrame, empdata::DataFrame)::DataFrame
+    joined = leftjoin(empdata, sims, on = [:dayn, :category])
 
     dists = DataFrame(p_row = joined[:, :p_row])
-    for name in [:area, :spore, :nl, :occup]
-        dists[!, Symbol(name, :_d)] .= absdiff.(joined[!, name], joined[!, Symbol(name, :_dat)], vars[Symbol(name, :_var)])
-        dists[!, Symbol(name, :_n)] .= findmissing.(joined[!, name], joined[!, Symbol(name, :_dat)])
+    for var in [:nlpct, :sporepct, :latentpct]
+        dists[!, var] .= absdiff.(joined[!, var], joined[!, Symbol(var, :_dat)])
+        dists[!, Symbol(var, :_n)] .= ismissing.(joined[!, var])
     end
-
-    # if 1 in sims.p_row
-        # CSV.write("results/ABC/dists/sents/q8/samplerawjoined.csv", joined)
-        # CSV.write("results/ABC/dists/sents/q8/samplerawdists.csv", dists)
-    # end
 
     sumdists = combine(groupby(dists, :p_row), Not(:p_row) .=> sum, renamecols = false)
 
     return sumdists
 end
 
-absdiff(sim::Float64, dat::Float64, var::Float64) = (sim - dat)^2 / var
-absdiff(sim::Missing, dat::Float64, var::Float64) = dat^2 / var
-absdiff(sim::Float64, dat::Missing, var::Float64) = 0.0
-absdiff(sim::Missing, dat::Missing, var::Float64) = 0.0
+absdiff(sim::Float64, dat::Float64) = (sim - dat)^2
+absdiff(sim::Missing, dat::Float64) = 1.0
 
-findmissing(sim::Float64, dat::Float64) = 0
-findmissing(sim::Missing, dat::Float64) = 1
-findmissing(sim::Float64, dat::Missing) = 0
-findmissing(sim::Missing, dat::Missing) = 0
+# function abs_norm_dist(sims::DataFrame, empdata::DataFrame, vars::DataFrameRow)::DataFrame
+#     joined = leftjoin(empdata, sims, on = [:plot, :dayn, :age, :cycle])
+
+#     any(ismissing.(joined.p_row)) && error("missing rows $(first(skipmissing(joined.p_row)))")
+
+#     dists = DataFrame(p_row = joined[:, :p_row])
+#     for name in [:area, :spore, :nl, :occup]
+#         dists[!, Symbol(name, :_d)] .= absdiff.(joined[!, name], joined[!, Symbol(name, :_dat)], vars[Symbol(name, :_var)])
+#         dists[!, Symbol(name, :_n)] .= findmissing.(joined[!, name], joined[!, Symbol(name, :_dat)])
+#     end
+
+#     # if 1 in sims.p_row
+#         # CSV.write("results/ABC/dists/sents/q8/samplerawjoined.csv", joined)
+#         # CSV.write("results/ABC/dists/sents/q8/samplerawdists.csv", dists)
+#     # end
+
+#     sumdists = combine(groupby(dists, :p_row), Not(:p_row) .=> sum, renamecols = false)
+
+#     return sumdists
+# end
+
+# absdiff(sim::Float64, dat::Float64, var::Float64) = (sim - dat)^2 / var
+# absdiff(sim::Missing, dat::Float64, var::Float64) = dat^2 / var
+# absdiff(sim::Float64, dat::Missing, var::Float64) = 0.0
+# absdiff(sim::Missing, dat::Missing, var::Float64) = 0.0
+
+# findmissing(sim::Float64, dat::Float64) = 0
+# findmissing(sim::Missing, dat::Float64) = 1
+# findmissing(sim::Float64, dat::Missing) = 0
+# findmissing(sim::Missing, dat::Missing) = 0
 
 ## Utils
 
-function count_obs(df::DataFrame)
-    missingscount = describe(df)[5:8,[:variable,:nmissing]]
-    return nrow(df) .- unstack(missingscount, :variable, :nmissing)
-end
+# function count_obs(df::DataFrame)
+#     missingscount = describe(df)[5:8,[:variable,:nmissing]]
+#     return nrow(df) .- unstack(missingscount, :variable, :nmissing)
+# end
 
-function scale_dists!(dists::DataFrame, counts::DataFrame)
-    # df = copy(dists)
-    # df[!, :p_row] .= dists[:, :p_row]
-    # for c in names(counts)
-        # df[!, Regex(c)] .= dists[!, Regex(c)] ./ counts[1, c]
-    for c in 1:ncol(counts)
-        dists[!, c+1] .= dists[!, c+1] ./ counts[1, c]
-    end
-    return dists
-end
+# function scale_dists!(dists::DataFrame, counts::DataFrame)
+#     # df = copy(dists)
+#     # df[!, :p_row] .= dists[:, :p_row]
+#     # for c in names(counts)
+#         # df[!, Regex(c)] .= dists[!, Regex(c)] ./ counts[1, c]
+#     for c in 1:ncol(counts)
+#         dists[!, c+1] .= dists[!, c+1] ./ counts[1, c]
+#     end
+#     return dists
+# end
